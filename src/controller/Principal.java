42b740de8557782debd6ee020eede862a8f5cfd3
diogofelix <diogofelix@localhost>
2024-06-23 17:44:48 -0300

    Primeiro commit

---
+++ src/controller/Principal.java
@@ -0,0 +1,34 @@
+package controller;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+        String filePath = "/tmp/exchange.json"; 
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+           
+            FileReader reader = new FileReader(filePath);
+            JsonObject jsonObject = JsonParser.parseReader(reader).getAsJsonObject();
+            reader.close();
+
+            System.out.print("Digite a unidade de moeda: ");
+            String unit = scanner.nextLine();
+
+            if (jsonObject.has("rates") && jsonObject.getAsJsonObject("rates").has(unit)) {
+                JsonObject unitObject = jsonObject.getAsJsonObject("rates").getAsJsonObject(unit);
+                System.out.println("Valores para " + unit + ": " + unitObject.toString());
+            } else {
+                System.out.println("Unidade de moeda não encontrada.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            scanner.close();
+        }
+    }
+}
---
+++ src/view/controller.java
@@ -0,0 +1,41 @@
+package view;
+import java.io.FileWriter;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class controller {
+    public static void main(String[] args) {
+        String urlString = "https://api.coingecko.com/api/v3/exchange_rates";
+        String outputPath = "/tmp/exchange.json"; 
+
+        try {
+            
+            URL url = new URL(urlString);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+              
+                InputStream inputStream = connection.getInputStream();
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+                try (OutputStream outputStream = Files.newOutputStream(Paths.get(outputPath))) {
+                    while ((bytesRead = inputStream.read(buffer)) != -1) {
+                        outputStream.write(buffer, 0, bytesRead);
+                    }
+                }
+                System.out.println("Arquivo salvo com sucesso em " + outputPath);
+            } else {
+                System.out.println("Erro na requisição: " + responseCode);
+            }
+            connection.disconnect();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
